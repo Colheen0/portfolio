130ac071ff1d995222c333e1300a5dfeb589e884
Colheen0 <colheenn@gmail.com>
2026-01-16 01:55:04 +0100

    changements de viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Ajoute cette ligne
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Ajoute cette ligne
+    tailwindcss(),
   ],
+  // Remplace 'ton-portfolio' par le NOM EXACT de ton dépôt sur GitHub
+  // Exemple : si ton URL est github.com/Colheen0/portfolio-2025, mets '/portfolio-2025/'
+  base: '/portfolio/', 
 })
